9e15689f4665af72e27dc3a7d2fa654e32b4e44d
Kevin A. Smith <lambada.calculus@gmail.com>
2023-02-20 10:28:37 -0500

    Reboot

--- lib/clik/renderable.ex
+++
@@ -1,5 +0,0 @@
-defprotocol Clik.Renderable do
-  @type error :: {:error, atom()}
-  @spec render(term()) :: iolist() | binary()
-  def render(t)
-end
--- test/support/show_help_command.ex
+++
@@ -1,7 +0,0 @@
-defmodule Clik.ShowHelpCommand do
-  use Clik.Command
-
-  def help_text(), do: "Display help"
-
-  def handle(_invocation), do: :help
-end
